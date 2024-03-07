fa6748941c77a934baf952522d0a52e1435e40f2
HubertKor21 <155633636+HubertKor21@users.noreply.github.com>
2024-03-07 14:48:04 +0100

    Strona

---
+++ Vanwiatrak/script.js
@@ -0,0 +1,58 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let moreArrow1 = document.querySelector(".more-arrow1");
+moreArrow1.onclick = function() {
+ navLinks.classList.toggle("show5");
+}
+let moreArrow2 = document.querySelector(".more-arrow2");
+moreArrow2.onclick = function() {
+ navLinks.classList.toggle("show6");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+let plArrow = document.querySelector(".pl-arrow");
+plArrow.onclick = function(){
+    navLinks.classList.toggle("show4");
+}
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
